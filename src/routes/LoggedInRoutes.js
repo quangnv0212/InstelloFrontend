0afb7a4c105c23256e8aad74757afeea9e4f63a6
Vu Quang <quangnv.0212@gmail.com>
2023-05-03 14:11:10 +0700

    fix lazy

--- src/routes/LoggedInRoutes.js
+++ src/routes/LoggedInRoutes.js
@@ -1,14 +1,14 @@
 import { Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-const Login = lazy(() => import("../pages/login"));
+import Login from "../pages/login";
 export default function LoggedInRoutes() {
   const { user } = useSelector((state) => ({ ...state }));
   return user ? (
     <Outlet />
   ) : (
-    <Suspense>
+    <>
       <Login />;
-    </Suspense>
+    </>
   );
 }
